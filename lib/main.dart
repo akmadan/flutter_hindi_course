dfdc989bd3f2534392b280c9731a2721fbfc2ef8
akmadan <akmadan1717@gmail.com>
2023-04-29 22:24:19 +0530

    lecture 1 - basic widgets

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hindi_course/pages/home_page.dart';
+import 'package:flutter_hindi_course/pages/login_page.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+
+
+
+
+
